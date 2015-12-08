6f36cccfee6dfbd14a228be688f3869f8c17a902
themarioga <marioga2512@gmail.com>
2015-12-08 20:50:16 +0100

    Fixed and slider

--- web-client/index.php
+++ web-client/index.php
@@ -13,15 +13,27 @@
 			<a href="javascript:void(0)" onclick="changeLRLayer('register')" class="waves-effect waves-light btn black white-text"><i class="material-icons left">add_box</i>Registrate!</a>
 		</div>
 		<div class="row center-align">
-			<div class="col s4 offset-s4">
+			<div class="col s8 m6 l4 offset-s2 offset-m3 offset-l4">
 				<div class="card blue-white lr-layer" id="lr-layer">
 					<?php include("pages/login.html"); ?>
 				</div>
 			</div>
 		</div>
+		<div class="slider fullscreen" style="z-index: -3; position: absolute;">
+			<ul class="slides">
+				<li>
+					<img src="resources/img/fondo.jpg">
+				</li>
+			</ul>
+		</div>
 		<script type="text/javascript" src="resources/js/jquery.min.js"></script>
 		<script type="text/javascript" src="resources/js/jquery.form.min.js"></script>
 		<script type="text/javascript" src="resources/js/materialize.min.js"></script>
 		<script type="text/javascript" src="resources/js/functions.js"></script>
+		<script>
+			$(document).ready(function(){
+				$('.slider').slider({full_width: true, indicators:false});
+			});
+		</script>
 	</body>
 </html>
